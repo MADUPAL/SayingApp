3c383beecb8bc5a68927e207b69a4bfa4fcd7149
MADUPAL <kevin488@naver.com>
2025-11-06 12:52:31 +0900

    -controller main에 있던 로직을 controller로 넘김

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,77 +1,15 @@
 package com.back;
 
-import com.back.say.MyHashMapSayRepository;
-import com.back.say.Say;
-import com.back.say.SayService;
+import com.back.say.*;
 
-import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        SayService mySayService = new SayService(new MyHashMapSayRepository());
-
-        System.out.println("== 명언 앱 ==");
-
-        while (true) {
-            System.out.print("명령) ");
-            String command = sc.nextLine();
-            if (command.equals("종료")) {
-                break;
-            } else if (command.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = sc.nextLine().trim();
-
-                System.out.print("작가 : ");
-                String author = sc.nextLine().trim();
-
-                int savedId = mySayService.save(author, content);
-                System.out.println(savedId + "번 명언이 등록되었습니다.");
-
-            } else if (command.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("=====================");
-                List<Say> allSays = mySayService.findAll();
-                for (Say say : allSays) {
-                    System.out.println(say);
-                }
-
-            } else if(command.contains("삭제")) {
-
-                int id = findIdFromCommand(command);
-                Say result = mySayService.findById(id);
-                if (result == null) {
-                    System.out.println(id + "번 명언은 존재하지 않습니다.");
-                    continue;
-                }
-                mySayService.delete(id);
-                System.out.println(id + "번 명언이 삭제되었습니다.");
-
-            } else if (command.contains("수정")) {
-                int id = findIdFromCommand(command);
-                Say result = mySayService.findById(id);
-                if (result == null) {
-                    System.out.println(id + "번 명언은 존재하지 않습니다.");
-                    continue;
-                }
-                System.out.println("명언(기존) : " + result.getContent());
-                System.out.print("명언 : ");
-                String updateContent = sc.nextLine();
-
-                System.out.println("작가(기존) : " + result.getAuthor());
-                System.out.print("작가 : ");
-                String updateAuthor = sc.nextLine();
-
-                mySayService.update(id, new Say(updateAuthor, updateContent));
-                System.out.println(id + "번 명언이 수정되었습니다.");
-            }
-        }
-        sc.close();
-    }
-    private static int findIdFromCommand(String command) {
-        return Integer.parseInt(command.substring(command.indexOf("=")+1));
+        SayRepository repository = new MyHashMapSayRepository();
+        SayService service = new SayService(repository);
+        new SayController(service, new Scanner(System.in)).run();
     }
 }
