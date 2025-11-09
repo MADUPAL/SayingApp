602240e724bf43c495eea1cf0e34168199453dc1
MADUPAL <kevin488@naver.com>
2025-11-09 22:18:29 +0900

    (9단계)
    -FileSayRepositoryV1
    명언을 db/wiseSaying에 .json파일로 저장하는 repository
    
    -Main
    repository가 FileSayRepositoryV1을 사용하도록 변경

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,7 +1,7 @@
 package com.back;
 
 import com.back.say.controller.SayController;
-import com.back.say.repository.InMemorySayRepository;
+import com.back.say.repository.FileSayRepositoryV1;
 import com.back.say.repository.SayRepository;
 import com.back.say.service.SayService;
 
@@ -11,7 +11,8 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        SayRepository repository = new InMemorySayRepository(); // 8단계
+        SayRepository repository = new FileSayRepositoryV1();
+//        SayRepository repository = new InMemorySayRepository(); // 8단계
         SayService service = new SayService(repository);
         new SayController(service, new Scanner(System.in)).run();
     }
